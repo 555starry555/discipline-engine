1f2f676dca9ed07c4c1bb43eaca2104cb1b701b7
555starry555 <1285688209@qq.com>
2026-06-03 09:44:04 +0800

    add lib/data/database/daos/point_log_dao.g.dart

---
+++ lib/data/database/daos/point_log_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'point_log_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$PointLogDaoMixin on DatabaseAccessor<AppDatabase> {
+  $PointLogTableTable get pointLogTable => attachedDatabase.pointLogTable;
+}
