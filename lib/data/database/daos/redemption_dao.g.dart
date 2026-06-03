e07958a219a85a36abbe6af0304af2e158d79281
555starry555 <1285688209@qq.com>
2026-06-03 09:44:08 +0800

    add lib/data/database/daos/redemption_dao.g.dart

---
+++ lib/data/database/daos/redemption_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'redemption_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$RedemptionDaoMixin on DatabaseAccessor<AppDatabase> {
+  $RedemptionTableTable get redemptionTable => attachedDatabase.redemptionTable;
+}
