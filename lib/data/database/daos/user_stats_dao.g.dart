baf0a85f2a5d8ab49ed02fe2b8312746e0a1c845
555starry555 <1285688209@qq.com>
2026-06-03 09:44:30 +0800

    add lib/data/database/daos/user_stats_dao.g.dart

---
+++ lib/data/database/daos/user_stats_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_stats_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$UserStatsDaoMixin on DatabaseAccessor<AppDatabase> {
+  $UserStatsTableTable get userStatsTable => attachedDatabase.userStatsTable;
+}
