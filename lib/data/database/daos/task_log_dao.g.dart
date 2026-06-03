5d14a9b52e9c64c508b779f8494c5b1039132a14
555starry555 <1285688209@qq.com>
2026-06-03 09:44:26 +0800

    add lib/data/database/daos/task_log_dao.g.dart

---
+++ lib/data/database/daos/task_log_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_log_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$TaskLogDaoMixin on DatabaseAccessor<AppDatabase> {
+  $TaskLogTableTable get taskLogTable => attachedDatabase.taskLogTable;
+  $TaskTableTable get taskTable => attachedDatabase.taskTable;
+}
